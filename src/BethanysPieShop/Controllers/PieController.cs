820badf32e92e3577d401dd3a24a65a8cdacf952
Alexander <alexiceanroid@gmail.com>
2017-12-04 23:08:39 +0300

    nice formatting

---
+++ src/BethanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            var piesListViewModel = new PiesListViewModel()
+            {
+                Pies = _pieRepository.Pies,
+                CurrentCategory = "Cheese cakes"
+            };
+            return View(piesListViewModel);
+        }
+    }
+}
